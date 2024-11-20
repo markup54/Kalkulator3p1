d398e1ea8c7134d4459597e71829b31bf9874ce9
markup54 <marzenakupka@tlen.pl>
2024-11-20 16:13:08 +0100

    Dodaj pliki projektów.

---
+++ Kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kalkulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //String liczba1 = liczba1TextBox.Text;
+            //int liczba1a = int.Parse(liczba1TextBox.Text);
+            int liczba1;
+            int liczba2;
+            int wynik = 0;
+            if (int.TryParse(liczba1TextBox.Text, out liczba1))
+            {
+                if (int.TryParse(liczba2TextBox.Text, out liczba2))
+                {
+                    wynik = liczba1+liczba2;
+                    MessageBox.Show("Wynik dodawania:"+wynik.ToString(), 
+                        "Wynik", MessageBoxButton.OK, MessageBoxImage.Information);
+                } 
+            }
+            else
+            {
+                MessageBox.Show("Niepoprawne dane","Komunikat",MessageBoxButton.OK, 
+                    MessageBoxImage.Warning);
+            }
+            
+        }
+    }
+}
